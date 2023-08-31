35bb0515402550194730b850aceab1e355cf4aba
Azh147852369 <56793754+Azh147852369@users.noreply.github.com>
2023-08-31 13:59:07 +0300

    Update Registration_steps.java

--- src/test/java/steps/Registration_steps.java
+++ src/test/java/steps/Registration_steps.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
